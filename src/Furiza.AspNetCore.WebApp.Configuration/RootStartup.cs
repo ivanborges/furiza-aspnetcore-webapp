7fddb4aad9d152eefa6b2ecc866f15ba6785bf5b
Ivan Borges <ivanborges@live.com>
2019-04-18 13:43:59 -0300

    Testing CICD.

--- src/Furiza.AspNetCore.WebApp.Configuration/RootStartup.cs
+++ src/Furiza.AspNetCore.WebApp.Configuration/RootStartup.cs
@@ -35,7 +35,7 @@ namespace Furiza.AspNetCore.WebApp.Configuration
 
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                //This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
